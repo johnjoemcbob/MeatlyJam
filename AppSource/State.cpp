409ec841d58ae0235b47fdee3033bb0e6c7ae5aa
johnjoemcbob <mcormack1994@hotmail.co.uk>
2015-03-27 17:39:59 +0000

    First Assets
    Sprite displays on screen

--- AppSource/State.cpp
+++ AppSource/State.cpp
@@ -29,13 +29,15 @@
 #include <State.h>
 
 #include <ChilliSource/Core/Base.h>
-#include <ChilliSource/Core/Entity.h>
 #include <ChilliSource/Core/Math.h>
-#include <ChilliSource/Core/Scene.h>
 #include <ChilliSource/Core/Resource.h>
+#include <ChilliSource/Core/Entity.h>
+#include <ChilliSource/Core/Scene.h>
+#include <ChilliSource/Core/State.h>
+
 #include <ChilliSource/Rendering/Base.h>
 #include <ChilliSource/Rendering/Camera.h>
-#include <ChilliSource/Rendering/Sprite.h>
+#include <ChilliSource/Rendering/Texture.h>
 #include <ChilliSource/Rendering/Material.h>
 
 namespace MeatlyJam
@@ -47,36 +49,38 @@ namespace MeatlyJam
 
 	void State::OnInit()
 	{
-		//Create the camera component
+		// Get a reference to the render component factory, to create the components
 		CSRendering::RenderComponentFactory* renderComponentFactory = CSCore::Application::Get()->GetSystem<CSRendering::RenderComponentFactory>();
-		CSRendering::CameraComponentSPtr cameraComponent = renderComponentFactory->CreatePerspectiveCameraComponent( CSCore::MathUtils::k_pi / 2.0f, 1.0f, 100.0f );
 
-		//create the camera entity and add the camera component
+		// Create the camera entity and add the camera component
 		Camera = CSCore::Entity::Create();
 		{
+			// Create the camera component
+			CSRendering::CameraComponentSPtr cameraComponent = renderComponentFactory->CreateOrthographicCameraComponent( CSCore::Vector2( 10, 10 ), 1.0f, 100.0f );
+
 			Camera->AddComponent( cameraComponent );
+			Camera->GetTransform().SetLookAt( CSCore::Vector3::k_unitNegativeZ * (f32) 10, CSCore::Vector3::k_zero, CSCore::Vector3::k_unitPositiveY );
 		}
 		GetScene()->Add( Camera );
 
-		//load the texture and texture atlas.
-		CSCore::ResourcePool* resourcePool = CSCore::Application::Get()->GetResourcePool();
-		CSRendering::TextureCSPtr texture = resourcePool->LoadResource<CSRendering::Texture>( CSCore::StorageLocation::k_package, "resources/textures/TextureAtlas.csimage" );
-		CSRendering::TextureAtlasCSPtr textureAtlas = resourcePool->LoadResource<CSRendering::TextureAtlas>( CSCore::StorageLocation::k_package, "resource/textures/TextureAtlas.csatlas" );
-
-		//create the material
-		CSRendering::MaterialFactory* materialFactory = CSCore::Application::Get()->GetSystem<CSRendering::MaterialFactory>();
-		CSRendering::MaterialSPtr material = materialFactory->CreateSprite( "SpriteMaterial", texture );
-
-		//Create the sprite component
-		CSRendering::RenderComponentFactory* renderComponentFactory = CSCore::Application::Get()->GetSystem<CSRendering::RenderComponentFactory>();
-		CSRendering::SpriteComponentSPtr spriteComponent = renderComponentFactory->CreateSpriteComponent( CSCore::Vector2::k_one, textureAtlas, "ImageId", material, CSRendering::SpriteComponent::SizePolicy::k_fitMaintainingAspect );
-
 		//create the sprite entity and add the sprite component
-		CSCore::EntitySPtr spriteEntity = CSCore::Entity::Create();
-		spriteEntity->AddComponent( spriteComponent );
+		Sprite_Test = CSCore::Entity::Create();
+		{
+			//load the texture and texture atlas.
+			CSCore::ResourcePool* resourcePool = CSCore::Application::Get()->GetResourcePool();
+			CSRendering::TextureCSPtr texture = resourcePool->LoadResource<CSRendering::Texture>( CSCore::StorageLocation::k_package, "Textures/UI.csimage" );
+			CSRendering::TextureAtlasCSPtr textureAtlas = resourcePool->LoadResource<CSRendering::TextureAtlas>( CSCore::StorageLocation::k_package, "Textures/UI.csatlas" );
 
-		//add the sprite to the scene
-		GetScene()->Add( spriteEntity );
+			//create the material
+			CSRendering::MaterialFactory* materialFactory = CSCore::Application::Get()->GetSystem<CSRendering::MaterialFactory>();
+			CSRendering::MaterialSPtr material = materialFactory->CreateSprite( "SpriteMaterial", texture );
+
+			//Create the sprite component
+			CSRendering::SpriteComponentSPtr spriteComponent = renderComponentFactory->CreateSpriteComponent( CSCore::Vector2( 1, 1 ), textureAtlas, "blue_button00", material, CSRendering::SpriteComponent::SizePolicy::k_fitMaintainingAspect );
+
+			Sprite_Test->AddComponent( spriteComponent );
+		}
+		GetScene()->Add( Sprite_Test );
 	}
 
 	void State::OnUpdate( f32 in_deltaTime )
